e68117c1fca8d16c6957c6859fb48e92691ad58a
Timas Ljungdahl <timas.ljungdahl@bontouch.com>
2023-05-08 20:54:45 +0200

    listen on socket in index.html

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('game connected');
+});
 
 server.listen(3000, () => {
     console.log('listening on localhost:3000');
--- public/src/powerups.js
+++ public/src/powerups.js
@@ -2,6 +2,6 @@ export const allPowerups = [
     {
         name: "frozen",
         id: 100,
-        color:"#FFFFFF"
+        color: "#FFFFFF"
     }
 ];
