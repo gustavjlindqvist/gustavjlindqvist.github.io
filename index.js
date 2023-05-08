915286139c25c212575b00a649e37a807b4a23d2
Timas Ljungdahl <timas.ljungdahl@bontouch.com>
2023-05-08 22:07:31 +0200

    Add functions to handle socket events

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const gameClientServer = new Server(server);
+
+//import { resetGame, startGame, setUpSelectPlayer, setSpeed } from './public/src/game.js'
 
 app.use(express.static('public'));
 
@@ -11,8 +13,28 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
+gameClientServer.on('connection', (socket) => {
     console.log('game connected');
+
+    socket.on('resetGame', () => {
+        console.log('reset game')
+    })
+
+    socket.on('startGame', () => {
+        console.log('start game')
+    })
+
+    socket.on('selectNumberOfPlayers', (data) => {
+        console.log('select number of players')
+    })
+
+    socket.on('gameSpeedSelector', (data) => {
+        console.log('select game speed')
+    })
+
+    socket.on('disconnect', () => {
+        console.log('game disconnected');
+    })
 });
 
 server.listen(3000, () => {
